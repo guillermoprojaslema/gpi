3c8b1a5f1703b313df7dcdc687403a3fb019ceb8
ljofre <ljofre2146@gmail.com>
2013-08-04 21:38:37 -0400

    clasificacion XYPT

--- core.R
+++ core.R
@@ -13,7 +13,20 @@ library(rioja) #clustering restringido
 path <- paste(getwd(),"/gpi/Copia de GPI (respuestas) - Respuestas de formulario (1).csv", sep="")
 df <- read.csv(file = path)
 data <- df[,3:length(df)]
-d = dist(x = data)
+d = dist(x = data, method="euclidean")
 
 h = hclust(d, method="ward", members=NULL)
 plot(h)
+
+# perfil X-Y
+XY = df[,c(19, 22, 25, 52)]
+PT = df[,c(15, 28, 46, 47, 49)]
+
+#clasificacion a priori que se contrastará con la clasificacion del modelo
+xyh = hclust(dist(XY), method="ward", members=NULL)
+pth = hclust(dist(PT), method="ward", members=NULL)
+
+XYPT = df[,c(19, 22, 25, 52, 15, 28, 46, 47, 49)]
+xypth = hclust(dist(XYPT), method="ward", members=NULL)
+plot(xypth)
+
