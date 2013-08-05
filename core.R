42cf4802104b87837100176b6a4065c402017351
ljofre <ljofre2146@gmail.com>
2013-08-04 20:31:12 -0400

    clasificacion en 3 preguntas

--- core.R
+++ core.R
@@ -1,21 +1,19 @@
-library(cluster)
+library(cluster) #clustering clasico
+library(tclust) #clustering restringido
+library(rioja) #clustering restringido
+
+# 1 - 12 Habilidades
+# 13 - 29 Habilidades o Talentos
+# 30 - 34 historia Biografica
+# 35 - 43 necesidades
+# 44 - 48 Factores ambientales 
+# 49 - 52 Perfil P T
+# 53 - 56 Perfil X Y
 
 path <- paste(getwd(),"/gpi/Copia de GPI (respuestas) - Respuestas de formulario (1).csv", sep="")
 df <- read.csv(file = path)
+data <- df[,3:length(df)]
+d = dist(x = data)
 
-df
-
-# clusterizacion (clasificacion)
-km <- pam(x=df[,3:length(df)], k=3)
-
-c = cor(df[,3:length(df)])
-plot(df[,3:20])
-
-#PERFIL X/Y
-#Si la persona o sujeto obtiene de las columnas 20,23,26,53 en total un rango de 13 a 20 punto es claramente del perfil Y. SI obtiene entre 4  a 12 Posee un perfil X
-#PERFIL P/T
-#Y de las columnas 16,47,48,50 si se obtiene un rango de 13 a 20 puntos de de perfil P y por lo tanto, entre 4 y 12 puntos un perfil T.
-#Y de las columnas 29, 47 si se obtiene un rango de 2 a 5 puntos de de perfil P y por lo tanto, entre 6 y 10 puntos un perfil T.
-# La pirámide es así
-#Estratégico : Los que son Y+P o Y+T. Táctico: X+T, Operacional X+P
-
+h = hclust(d, method="ward", members=NULL)
+plot(h)
--- presentacion.md
+++ presentacion.md
@@ -1,6 +1,11 @@
 Clasificacion de aptitudes
 ========================================================
 
+### Antecedentes
+La muestra se obtuvo por medio de encuestas por internet
+
+
+### Clasificación X/Y
 tipo X: Trabajan lo menos posible, carecen de ambición, evitan responsabilidades, prefieren que las manden, se resisten a los cambios, son credulas o están mal informadas, harían muy poco por la empresa si no fuera por la dirección.
 
 tipo Y: Relacionan el trabajo con el juego
@@ -18,3 +23,13 @@ df <- read.csv(file = path)
 df
 ```
 
+
+
+#PERFIL X/Y
+#Si la persona obtiene de las columnas 20,23,26,53 en total un rango de 13 a 20 punto es del perfil Y. SI obtiene entre 4 a 12 Posee un perfil X.
+
+#PERFIL P/T
+#Y de las columnas 16,47,48,50 si se obtiene un rango de 13 a 20 puntos de de perfil P y por lo tanto, entre 4 y 12 puntos un perfil T.
+#Y de las columnas 29, 47 si se obtiene un rango de 2 a 5 puntos de de perfil P y por lo tanto, entre 6 y 10 puntos un perfil T.
+# La pirámide es así
+#Estratégico : Los que son Y+P o Y+T. Táctico: X+T, Operacional X+P
